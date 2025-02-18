b53ab0b9bf8a809d22f846fd67bb77db18fd4fd2
emekach <chukwuemeka.chukwuma11@gmail.com>
2025-02-18 11:26:26 +0000

    fix: fixed vercel deploy config

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,15 +1,10 @@
 const express = require('express');
-const { processTick } = require('../controllers/tickController');
+
 const path = require('path');
 
 const router = express.Router();
 
-// Serve integration.json
-router.get('/integration.json', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '../config/integration.json'));
-});
-
-// Tick endpoint
-router.post('/tick', processTick);
-
+module.exports = (req, res) => {
+  res.sendFile(path.resolve('../config/integration.json'));
+};
 module.exports = router;
